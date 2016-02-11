abcef183cffe92b518881f6ec52fd7044c81c160
Guest User <student@epicodus.com>
2016-02-10 19:59:35 -0800

    makes interactive rides page

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  // var height = parseInt(prompt("how tall are you?"));
+  //
+  // if (height >= 4) {
+  //   $("#big-kid")background-color();
+  // } else {
+  //   $("#small-kid").show();
+  // }
+});
